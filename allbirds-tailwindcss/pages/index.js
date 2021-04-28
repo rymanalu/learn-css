ecb1a48839d5645e97f6aa804e087575acedee0e
Roni Yusuf <roni.yusuf@finaccel.co>
2021-04-28 14:51:29 +0700

    02 (Part one finished)

--- allbirds-tailwindcss/pages/index.js
+++ allbirds-tailwindcss/pages/index.js
@@ -10,6 +10,69 @@ export default function Home() {
     `Weather Repellent Shoes`,
   ]
 
+  const products = [
+    {
+      name: "Women's Wool Runner Mizzles",
+      description: 'Our weather-ready sneaker made with merino wool and Puddle Guard™.',
+      styles: [
+        {
+          name: 'Natural Gray (Cream Sole)',
+          price: '$115',
+          colors: ['#585c60', '#e2e2da'],
+          image: 'https://cdn.allbirds.com/image/fetch/q_auto,f_auto/w_420,f_auto,q_auto,b_rgb:f5f5f5/https://cdn.shopify.com/s/files/1/1104/4168/products/WR3WNCW_SHOE_ANGLE_GLOBAL_WOMENS_WOOL_RUNNER_GREY_LIGHT_GREY_bca9b775-cdd3-4bf2-afef-97935ea55cf8.png%3Fv=1610049516'
+        },
+        {
+          name: 'Savanna Night (Cream Sole)',
+          price: '$115',
+          colors: ['#353946', '#e2e2da'],
+          image: 'https://cdn.allbirds.com/image/fetch/q_auto,f_auto/w_420,f_auto,q_auto,b_rgb:f5f5f5/https://cdn.shopify.com/s/files/1/1104/4168/products/WR3WNCW_SHOE_ANGLE_GLOBAL_WOMENS_WOOL_RUNNER_GREY_LIGHT_GREY_bca9b775-cdd3-4bf2-afef-97935ea55cf8.png%3Fv=1610049516'
+        },
+        {
+          name: 'Black Sands (Asphalt Sole)',
+          price: '$115',
+          colors: ['#28282c', '#434345'],
+          image: 'https://cdn.allbirds.com/image/fetch/q_auto,f_auto/w_420,f_auto,q_auto,b_rgb:f5f5f5/https://cdn.shopify.com/s/files/1/1104/4168/products/WR3WNCW_SHOE_ANGLE_GLOBAL_WOMENS_WOOL_RUNNER_GREY_LIGHT_GREY_bca9b775-cdd3-4bf2-afef-97935ea55cf8.png%3Fv=1610049516'
+        },
+        {
+          name: 'Cardamom (Cream Sole)',
+          price: '$115',
+          colors: ['#9d957a', '#e2e2da'],
+          image: 'https://cdn.allbirds.com/image/fetch/q_auto,f_auto/w_420,f_auto,q_auto,b_rgb:f5f5f5/https://cdn.shopify.com/s/files/1/1104/4168/products/WR3WNCW_SHOE_ANGLE_GLOBAL_WOMENS_WOOL_RUNNER_GREY_LIGHT_GREY_bca9b775-cdd3-4bf2-afef-97935ea55cf8.png%3Fv=1610049516'
+        },
+        {
+          name: 'Ginseng (Beige Sole)',
+          price: '$115',
+          colors: ['#e7e5e2', '#bfbab5'],
+          image: 'https://cdn.allbirds.com/image/fetch/q_auto,f_auto/w_420,f_auto,q_auto,b_rgb:f5f5f5/https://cdn.shopify.com/s/files/1/1104/4168/products/WR3WNCW_SHOE_ANGLE_GLOBAL_WOMENS_WOOL_RUNNER_GREY_LIGHT_GREY_bca9b775-cdd3-4bf2-afef-97935ea55cf8.png%3Fv=1610049516'
+        },
+        {
+          name: 'Dapple Gray (Beige Sole)',
+          price: '$115',
+          colors: ['#e7e5e2', '#bfbab5'],
+          image: 'https://cdn.allbirds.com/image/fetch/q_auto,f_auto/w_420,f_auto,q_auto,b_rgb:f5f5f5/https://cdn.shopify.com/s/files/1/1104/4168/products/WR3WNCW_SHOE_ANGLE_GLOBAL_WOMENS_WOOL_RUNNER_GREY_LIGHT_GREY_bca9b775-cdd3-4bf2-afef-97935ea55cf8.png%3Fv=1610049516'
+        },
+        {
+          name: 'Aurora (Cream)',
+          price: '$115',
+          colors: ['#e7e5e2', '#bfbab5'],
+          image: 'https://cdn.allbirds.com/image/fetch/q_auto,f_auto/w_420,f_auto,q_auto,b_rgb:f5f5f5/https://cdn.shopify.com/s/files/1/1104/4168/products/WR3WNCW_SHOE_ANGLE_GLOBAL_WOMENS_WOOL_RUNNER_GREY_LIGHT_GREY_bca9b775-cdd3-4bf2-afef-97935ea55cf8.png%3Fv=1610049516'
+        },
+        {
+          name: 'Pacific (Cream Sole)',
+          price: '$115',
+          colors: ['#e7e5e2', '#bfbab5'],
+          image: 'https://cdn.allbirds.com/image/fetch/q_auto,f_auto/w_420,f_auto,q_auto,b_rgb:f5f5f5/https://cdn.shopify.com/s/files/1/1104/4168/products/WR3WNCW_SHOE_ANGLE_GLOBAL_WOMENS_WOOL_RUNNER_GREY_LIGHT_GREY_bca9b775-cdd3-4bf2-afef-97935ea55cf8.png%3Fv=1610049516'
+        },
+        {
+          name: 'True Black (Black Sole)',
+          price: '$115',
+          colors: ['#e7e5e2', '#bfbab5'],
+          image: 'https://cdn.allbirds.com/image/fetch/q_auto,f_auto/w_420,f_auto,q_auto,b_rgb:f5f5f5/https://cdn.shopify.com/s/files/1/1104/4168/products/WR3WNCW_SHOE_ANGLE_GLOBAL_WOMENS_WOOL_RUNNER_GREY_LIGHT_GREY_bca9b775-cdd3-4bf2-afef-97935ea55cf8.png%3Fv=1610049516'
+        }
+      ]
+    }
+  ]
+
   return (
     <div className="font-poppins">
       <Head>
@@ -20,7 +83,7 @@ export default function Home() {
           We're raising prices on all products by $1 today in support of the planet.{` `}<a href="#" className="underline">Learn More</a>.
         </p>
       </div>
-      <header className="relative px-5 py-3 flex items-center justify-between">
+      <header className="z-10 sticky top-0 px-5 py-3 flex items-center justify-between bg-white">
         <div className="absolute inset-0 shadow-lg opacity-50"></div>
         <div className="flex">
           <button className="h-8 w-8">
@@ -54,7 +117,7 @@ export default function Home() {
             </div>
             <div>
               <button className="block h-6 w-6">
-                <svg className="transform rotate-90" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <svg className="z-0 transform rotate-90" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1} d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4" />
                 </svg>
               </button>
@@ -89,8 +152,50 @@ export default function Home() {
               </p>
             </div>
           </div>
-          <div>
-
+          <div className="py-12 px-8">
+            {products.map((product, i) => (
+              <div key={i}>
+                <div>
+                  <h2 className="text-lg font-semibold">{product.name}</h2>
+                  <p className="text-sm text-gray-800">{product.description}</p>
+                </div>
+                <div className="mt-4 shadow-xl">
+                  <div className="shadow-lg">
+                    <a href="#">
+                      <div>
+                        <img src={product.styles[0].image} />
+                      </div>
+                      <div className="px-4 pt-3">
+                        <h3 className="text-sm font-semibold">{product.name}</h3>
+                        <p className="mt-1 text-sm text-gray-800">{product.styles[0].name}</p>
+                      </div>
+                    </a>
+                    <div className="mt-1 px-4 pb-4">
+                      <p className="text-sm text-gray-800">{product.styles[0].price}</p>
+                      <div className="mt-4 flex space-x-4">
+                        {product.styles.slice(0, 6).map((style, j) => (
+                          <button key={j} className={`h-6 w-6 flex flex-col border border-gray-300 rounded-full overflow-hidden transform -rotate-45 ${j === 0 ? 'ring-2 ring-offset-1 ring-gray-300' : ''}`}>
+                            <span className="sr-only">{style.name}</span>
+                            <span className="h-3 w-6" style={{ backgroundColor: style.colors[0] }}></span>
+                            <span className="h-3 w-6" style={{ backgroundColor: style.colors[1] }}></span>
+                          </button>
+                        ))}
+                      </div>
+                    </div>
+                    <div className="border-t">
+                      <button className="px-4 py-2 text-sm w-full flex justify-between">
+                        <span className="font-semibold">Quick Add</span>
+                        <span>
+                          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
+                          </svg>
+                        </span>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </main>
